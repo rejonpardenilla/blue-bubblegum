4c8e5c4345c26a22f0a47c9e4079562d7c8f2628
Ricardo Sosals <rechi.sosa09@gmail.com>
2017-04-16 05:18:56 -0500

    First commimt

---
+++ catalog/js/catalog.js
@@ -0,0 +1,9 @@
+const addProductButton = document.getElementById('addProductButton')
+const editProductButton = document.getElementById('editProductButton')
+const removeProductButton = document.getElementById('removeProductButton')
+
+addProductButton.addEventListener('click', )
+
+function addProduct () {
+	
+}
