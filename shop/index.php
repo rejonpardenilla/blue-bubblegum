c8e5ed50d55f8ace466963837d50924f056cf550
Daniel Rejon <rejonpardenilla@gmail.com>
2017-04-17 19:58:40 -0500

    initial design

---
+++ shop/index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Blue Bubblegum</title>
+  <link rel="stylesheet" href="shop/css/shop.css">
+</head>
+<body>
+  <header>
+    <div id="logo">
+      <a href="#">
+        Blue Bubblegum
+      </a>
+    </div>
+  
+    <div id="categories-menu">
+      <ul>
+        <li><a href="#">TECNOLOGIA</a></li>
+        <li><a href="#">COMICS &amp; MANGA</a></li>
+        <li><a href="#">PELICULAS</a></li>
+        <li><a href="#">OTROS</a></li>
+      </ul>
+    </div>
+
+    <div id="login-menu">
+      <a href="#">
+        login
+      </a>
+    </div>
+  </header>
+
+  <section id="content">
+    <div class="sticker">
+      <img src="http://orm-other.s3.amazonaws.com/progjsappssplash/cover.jpg" alt="book">
+      <span class="title">Javascript programming</span>
+      <span class="price">$9.99</span>
+    </div>
+
+    <div class="sticker">
+      <img src="http://orm-other.s3.amazonaws.com/progjsappssplash/cover.jpg" alt="book">
+      <span class="title">Javascript programming</span>
+      <span class="price">$9.99</span>
+    </div>
+
+    <div class="sticker">
+      <img src="http://orm-other.s3.amazonaws.com/progjsappssplash/cover.jpg" alt="book">
+      <span class="title">Javascript programming</span>
+      <span class="price">$9.99</span>
+    </div>
+
+    <div class="sticker">
+      <img src="http://orm-other.s3.amazonaws.com/progjsappssplash/cover.jpg" alt="book">
+      <span class="title">Javascript programming</span>
+      <span class="price">$9.99</span>
+    </div>
+
+    <div class="sticker">
+      <img src="http://orm-other.s3.amazonaws.com/progjsappssplash/cover.jpg" alt="book">
+      <span class="title">Javascript programming</span>
+      <span class="price">$9.99</span>
+    </div>
+
+    
+  </section>
+
+  <footer>
+
+  </footer>
+</body>
+</html>
