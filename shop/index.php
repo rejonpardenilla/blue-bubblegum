14e2c4e747c4448fe57fd7f1ea93582abeb764a2
Daniel Rejon <rejonpardenilla@gmail.com>
2017-04-18 14:39:08 -0500

    add better samples

--- shop/index.php
+++ shop/index.php
@@ -31,34 +31,37 @@
 
   <section id="content">
     <div class="sticker">
-      <img src="http://orm-other.s3.amazonaws.com/progjsappssplash/cover.jpg" alt="book">
-      <span class="title">Javascript programming</span>
-      <span class="price">$9.99</span>
+      <img src="https://ih0.redbubble.net/image.357542102.4834/st%2Csmall%2C420x460-pad%2C420x460%2Cf8f8f8.lite-1u2.jpg" alt="sticker">
+      <span class="title">Bored Seal</span>
+      <span class="price">$2.53</span>
+    </div>
+    
+    <div class="sticker">
+      <img src="https://ih1.redbubble.net/image.353367608.9428/st%2Csmall%2C420x460-pad%2C420x460%2Cf8f8f8.lite-1u2.jpg" alt="sticker">
+      <span class="title">Smart Unicorn</span>
+      <span class="price">$3.80</span>
     </div>
 
     <div class="sticker">
-      <img src="http://orm-other.s3.amazonaws.com/progjsappssplash/cover.jpg" alt="book">
-      <span class="title">Javascript programming</span>
-      <span class="price">$9.99</span>
+      <img src="https://ih1.redbubble.net/image.331577825.1978/st%2Csmall%2C215x235-pad%2C210x230%2Cf8f8f8.lite-1.jpg" alt="sticker">
+      <span class="title">VueJS</span>
+      <span class="price">$2.95</span>
     </div>
 
     <div class="sticker">
-      <img src="http://orm-other.s3.amazonaws.com/progjsappssplash/cover.jpg" alt="book">
-      <span class="title">Javascript programming</span>
-      <span class="price">$9.99</span>
+      <img src="https://ih0.redbubble.net/image.333500281.9085/st%2Csmall%2C420x460-pad%2C420x460%2Cf8f8f8.lite-1.jpg" alt="sticker">
+      <span class="title">Loving cats</span>
+      <span class="price">$2.53</span>
     </div>
 
     <div class="sticker">
-      <img src="http://orm-other.s3.amazonaws.com/progjsappssplash/cover.jpg" alt="book">
-      <span class="title">Javascript programming</span>
-      <span class="price">$9.99</span>
+      <img src="https://ih0.redbubble.net/image.258761544.3923/st%2Csmall%2C215x235-pad%2C210x230%2Cf8f8f8.lite-1.jpg" alt="sticker">
+      <span class="title">Ninja dev set</span>
+      <span class="price">$3.80</span>
     </div>
 
-    <div class="sticker">
-      <img src="http://orm-other.s3.amazonaws.com/progjsappssplash/cover.jpg" alt="book">
-      <span class="title">Javascript programming</span>
-      <span class="price">$9.99</span>
-    </div>
+
+    
 
     
   </section>
