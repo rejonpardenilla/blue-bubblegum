7c57032cd7482bcca79db88806f6be06d101c131
Raul Canul <eduardoct120@gmail.com>
2017-04-18 21:49:26 -0500

    Register users on back end implemented

---
+++ src/RegisterUser.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ *
+ */
+include ( 'utils/AdminDB.php' );
+
+class RegisterUser {
+
+  private $adminDB;
+
+  function __construct() {
+    # code...
+    $this->adminDB = new AdminDB();
+  }
+
+  public function register( $userData ) {
+    $table = 'users';
+    $this->adminDB->insertar( $table, $userData );
+
+    $_SESSION[ 'name' ] = $userData[ 'name' ];
+    $_SESSION[ 'email' ] = $userData[ 'email' ];
+
+  }
+}
+
+function Main() {
+  $userName = $_POST[ 'name' ];
+  $userEmail = $_POST[ 'email' ];
+  $userPassword = $_POST[ 'password' ];
+
+  $userData = array(  'name' => $userName ,
+                      'email' => $userEmail,
+                      'password' => $userPassword );
+
+  $registerUser = new RegisterUser();
+  $registerUser->register( $userData );
+}
+
+Main()
+
+
+ ?>
